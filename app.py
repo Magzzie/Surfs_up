f0cd2f5fa000d70cd21f263eefa774be34ff147c
Magzzie <99442686+Magzzie@users.noreply.github.com>
2022-04-24 21:49:06 -0400

    updated flask app to include challenge, updates graphs.

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
+from sqlalchemy import extract
 
 # Import dependencies for Flask
 from flask import Flask, jsonify
@@ -43,7 +44,11 @@ def welcome():
         /api/v1.0/precipitation<br/>
         /api/v1.0/stations<br/>
         /api/v1.0/tobs <br/>
-        /api/v1.0/temp/start/end 
+        /api/v1.0/temp/start/end <br/>
+        /api/v1.0/temps/junes <br/>
+        /api/v1.0/stats/junes <br/>
+        /api/v1.0/temps/decembers <br/>
+        /api/v1.0/stats/decembers
         ''')
 
 # Create a separate route for the precipitation analysis for the last year. 
@@ -90,3 +95,39 @@ def stats(start=None, end=None):
         filter(Measurement.date <= end).all()
     temps = list(np.ravel(results))
     return jsonify(temps)
+
+# Create a route for the temperatures of June over the years (challenge, deliverable 1).
+@app.route("/api/v1.0/temps/junes")
+def june_temps():
+    results = session.query(Measurement.date, Measurement.tobs).\
+        filter(extract('month', Measurement.date) == 6).all()
+    june_temp = {date: tobs for date, tobs in results}
+    return jsonify(june_temp)
+
+# Create a route for the summary statistics of June over the years (challenge, deliverable 1).
+@app.route("/api/v1.0/stats/junes")
+def june_stats():
+    sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
+
+    results = session.query(*sel).\
+        filter(extract('month', Measurement.date) == 6).all()
+    june_stats = list(np.ravel(results))
+    return jsonify(june_stats)
+
+# Create a route for the temperatures of December over the years (challenge, deliverable 2).
+@app.route("/api/v1.0/temps/decembers")
+def dec_temps():
+    results = session.query(Measurement.date, Measurement.tobs).\
+        filter(extract('month', Measurement.date) == 12).all()
+    dec_temp = {date: tobs for date, tobs in results}
+    return jsonify(dec_temp)
+
+# Create a route for the summary statistics of December over the years (challenge, deliverable 2).
+@app.route("/api/v1.0/stats/decembers")
+def dec_stats():
+    sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
+
+    results = session.query(*sel).\
+        filter(extract('month', Measurement.date) == 12).all()
+    dec_stats = list(np.ravel(results))
+    return jsonify(dec_stats)
